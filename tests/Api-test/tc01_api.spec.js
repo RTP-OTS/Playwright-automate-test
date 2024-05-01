10a8333f6018160ec40b7ed9b885a4a2ddc810cd
RTP-OTS <rattapon.ot@gmail.com>
2024-05-01 23:12:09 +0700

    Update tc01_api.spec.js

--- tests/Api-test/tc01_api.spec.js
+++ tests/Api-test/tc01_api.spec.js
@@ -1,8 +1,4 @@
 const { test , expect } = require ('@playwright/test');
-const { request } = require('http');
-const { generateName }=require ('../../page/utils_generateName');
-
-const enFirstname= generateName();
 var userid
 
 test ("Get User" , async ({request}) => {
@@ -26,25 +22,25 @@ test ("Create User" , async ({request}) => {
         userid=res.id
     })
 
-// test ("Update User and verify message after this" , async ({request}) => {
-//     const resonseUpdate =await request.put('https://reqres.in/api/users/'+ userid,
-//     {
-//         data:{ "name":"Toon" ,
-//          "job": "QA Automated" },
-//         headers:{"Accept":"application/json"}
+test ("Update User and verify message after this" , async ({request}) => {
+    const resonseUpdate =await request.put('https://reqres.in/api/users/'+ userid,
+    {
+        data:{ "name":"Toon" ,
+         "job": "QA Automated" },
+        headers:{"Accept":"application/json"}
 
-//         });
-//         console.log(await resonseUpdate.json())
-//         expect(resonseUpdate.status()).toBe(200)
+        });
+        console.log(await resonseUpdate.json())
+        expect(resonseUpdate.status()).toBe(200)
 
-//         const jsonResponse = await resonseUpdate.json()
-//         expect(jsonResponse.name).toContain('Toon')
-//         expect(jsonResponse.job).toContain('QA Automated')
-//     })
+        const jsonResponse = await resonseUpdate.json()
+        expect(jsonResponse.name).toContain('Toon')
+        expect(jsonResponse.job).toContain('QA Automated')
+    })
 
-// test ("Delete user after update " , async ({request}) => {
-//     const responseUser = await request.delete('https://reqres.in/api/users'+userid,)
-//         expect(responseUser.status()).toBe(204)
-// })
+test ("Delete user after update " , async ({request}) => {
+    const responseUser = await request.delete('https://reqres.in/api/users'+userid,)
+        expect(responseUser.status()).toBe(204)
+})
 
 
