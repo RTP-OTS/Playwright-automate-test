638a0c0803d2808e0a0b8db884369db9a781c298
RTP-OTS <rattapon.ot@gmail.com>
2024-05-18 20:24:49 +0700

    update

--- tests/api/userTests.spec.js
+++ tests/api/userTests.spec.js
@@ -1,26 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-const baseURL = 'https://reqres.in';
 test.describe('User API Tests', () => {
     let userId; 
 
     test('POST - Create User', async ({ request }) => {
-        const response = await request.post(`${baseURL}/api/user/`,{
-            data:{ "name":"Sea",
+        const response = await request.post('https://reqres.in/api/user/',{
+            data:{
+                "name":"Sea",
                 "job": "Archer"}
         });
         expect(response.status()).toBe(201);
         const responseBody = await response.json();
-        expect(responseBody.name).toEqual("Sea")
+        expect(responseBody.name).toEqual("Sea");
         expect(responseBody.job).toEqual("Archer");
         expect(responseBody).toHaveProperty('id');
 
-        // Prepare UserId for used
+        // Keep user id for used
         userId = responseBody.id;
       });
 
-    test('Put - Update User', async ({ request }) =>{
-        const response = await request.put(`${baseURL}/api/user/${userId}`, {
+      test('Put - Update User', async ({ request }) =>{
+        const response = await request.put('https://reqres.in/api/user/'+userId, {
             data : {
                 "name":"Sky",
                 "job":"Hunter"
@@ -31,9 +31,5 @@ test.describe('User API Tests', () => {
         expect(responseBody.name).toEqual("Sky")
         expect(responseBody.job).toEqual("Hunter");
     })
-
-    test('Delete - Delete User', async ({ request }) =>{
-        const response = await request.delete(`${baseURL}/api/user/${userId}`)
-        expect(response.status()).toBe(204)
-    })
+      
 });
